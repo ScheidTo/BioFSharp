2b906ea6ce4e9fabe4d5bd6de95127210e269c25
ScheidTo <tobscheid@web.de>
2024-04-25 15:55:12 +0200

    with record type

--- src/BioFSharp.Stats/RNASeq.fs
+++ src/BioFSharp.Stats/RNASeq.fs
@@ -5,6 +5,11 @@ open System.Collections.Generic
 
 
 module RNASeq = 
+    type RNASeqInput = { // type construction
+    GeneID : string
+    GeneLength : float
+    GeneCount : float
+    } with static member Create id gl gc = {GeneID=id;GeneLength=gl;GeneCount=gc}
     //module Rpkm =
     let calcRPM sumOfAllReadsPerMil counts =
         (counts |> float) / sumOfAllReadsPerMil
@@ -12,33 +17,46 @@ module RNASeq =
     let calcRPKM geneLength rpm = 
         (float rpm) / ((float geneLength) / 1000.) 
 
-    let rpkms (geneLengthAndCounts:seq<string*(float*float)>) =
+    let rpkms (geneLengthAndCounts:seq<RNASeqInput>) =
         let sumOfAllReads = 
-            geneLengthAndCounts
-            |> Seq.map (fun (x,(y,z)) -> z)
-            |> Seq.sum
+                geneLengthAndCounts
+                |> Seq.map (fun geneLengthAndCounts -> geneLengthAndCounts.GeneCount)
+                |> Seq.sum
         let sumOfAllReadsPerMil =
             sumOfAllReads / 1000000. 
-        let rpms = 
+        let rpms =
             geneLengthAndCounts
-            |> Seq.map (fun (head,(l,c)) -> head, (l, calcRPM sumOfAllReadsPerMil c))
-        let rpkms = 
-            rpms //geneLengthAndCounts // should be rpms i think
-            |> Seq.map (fun (head,(l,c)) -> head, calcRPKM l c)
-        rpkms
-
+            |> Seq.map (fun geneLengthAndCounts -> calcRPM sumOfAllReadsPerMil geneLengthAndCounts.GeneCount)
+        let rpkms =
+            let geneLengthsRPM = 
+                geneLengthAndCounts
+                |> Seq.map (fun getLength -> getLength.GeneLength)
+            let rpkm =
+                Seq.zip geneLengthsRPM rpms
+                |> Seq.map (fun (length, rpm) -> calcRPKM length rpm)
+            rpkm
+        let geneID =
+            geneLengthAndCounts
+            |> Seq.map (fun geneLengthAndCounts -> geneLengthAndCounts.GeneID)
+        let rpkmResult =
+            Seq.zip geneID rpkms
+        rpkmResult
     //module Tpm =
-    let tpms (geneLengthAndCounts:seq<string*(float*float)>) =
+    let tpms (geneLengthAndCounts:seq<RNASeqInput>) =
         let rpk = 
             geneLengthAndCounts
-            |> Seq.map (fun (head,(l,c)) -> head, c/l/1000.)
+            |> Seq.map (fun geneLengthAndCounts -> geneLengthAndCounts.GeneCount/geneLengthAndCounts.GeneLength/1000.)
         let sumOfAllReads =
             rpk
-            |> Seq.map snd
             |> Seq.sum
         let sumOfAllReadsPerMil =
-            sumOfAllReads / 1000000. 
+            sumOfAllReads / 1000000.
         let tpms =
             rpk
-            |> Seq.map (fun (header,rpks) -> header, rpks/sumOfAllReadsPerMil)
-        tpms
+            |> Seq.map (fun rpks -> rpks/sumOfAllReadsPerMil)
+        let geneID =
+            geneLengthAndCounts
+            |> Seq.map (fun geneLengthAndCounts -> geneLengthAndCounts.GeneID)
+        let tpmResult =
+            Seq.zip geneID tpms
+        tpmResult
--- tests/BioFSharp.Tests/BioFSharp.Stats/RNASeqTests.fs
+++ tests/BioFSharp.Tests/BioFSharp.Stats/RNASeqTests.fs
@@ -6,6 +6,10 @@ open Expecto
 
 //let testSeq = seq { for i in 1000. .. 1019. -> ("stringtest"+ i.ToString(),(100.,i))}
 let testSeq = seq { for i in 1. .. 2. -> ("stringtest"+ i.ToString(),(i,i))}
+let testgeneID = seq { "stringtest1";  "stringtest2"}
+let testLength = seq {1.; 2.}
+let testCount = seq {1.;2.}
+let testInSeq = Seq.map3 (fun id gl gc ->  RNASeq.RNASeqInput.Create id gl gc) testgeneID testLength testCount
 
 let resultRPKM = seq { ("stringtest1", 333333333.3333333); ("stringtest2", 333333333.3333333)}
 let resultTPM = seq {("stringtest1", 500000.); ("stringtest2", 500000.)}
@@ -16,7 +20,7 @@ let RNASeqTests =
     testList "RNASeqTests" [
         testCase "RPKM" (fun _ ->
             Expect.equal 
-                (RNASeq.rpkms testSeq
+                (RNASeq.rpkms testInSeq
                 |> Seq.map (fun (x,y) -> x, System.Math.Round (y,4))|> Array.ofSeq)
                 (resultRPKM
                 |> Seq.map (fun (x,y) -> x, System.Math.Round (y,4)) |> Array.ofSeq)
@@ -24,7 +28,7 @@ let RNASeqTests =
         )
         testCase "TPM" (fun _ ->
             Expect.equal 
-                (RNASeq.tpms testSeq 
+                (RNASeq.tpms testInSeq 
                 |> Seq.map (fun (x,y) -> x, System.Math.Round (y,4))|> Array.ofSeq)
                 (resultTPM
                 |> Seq.map (fun (x,y) -> x, System.Math.Round (y,4))|> Array.ofSeq)
